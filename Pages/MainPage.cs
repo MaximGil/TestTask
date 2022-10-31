7eb371299010e4b321369c8f8fb302f27096ce5a
Maksim Gil <gil00maksim@gmail.com>
2022-10-31 10:49:41 +0300

    Add tests.

--- Pages/MainPage.cs
+++ Pages/MainPage.cs
@@ -13,12 +13,22 @@ public class MainPage : BasePage
     private ILocator ResultLocator => Page.Locator("[id='resultDiv']");
     private ILocator TitleLocator => Page.Locator("xpath=//h1[contains(@class, 'text-center')]");
 
+    
+    public async Task Calculate(string number)
+    {     
+            await InputField.FillAsync(number);
+            await CalculateButton.ClickAsync();  
+    }
 
+    public async Task<string> GetResult()
+    {
+        await ResultLocator.WaitForAsync(new LocatorWaitForOptions() {State = WaitForSelectorState.Visible});
+        return await ResultLocator.InnerTextAsync();
+    }
+    
     public async Task<bool> IsPageDisplayed()
     {
         return await InputField.IsEnabledAsync() && await CalculateButton.IsEnabledAsync() &&
                await ResultLocator.IsEnabledAsync() && await TitleLocator.IsEnabledAsync();
     }
-
-
 }
---
+++ TestTask/MainPageTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Playwright.NUnit;
+using Pages;
+
+namespace TestTask;
+
+public class MainPageTests : PageTest
+{
+    private MainPage? _mainPage;
+    private string _title = "The greatest factorial calculator!";
+    private string Result(string number, string count) => $"The factorial of {number} is: {count}";
+    private readonly string _errorMessage = "Please enter an integer";
+
+    [SetUp]
+    public async Task Setup()
+    {
+        _mainPage = new MainPage(Page);
+        await _mainPage.Goto();
+    }
+
+    [Test]
+    public async Task PageIsEnabled()
+    {
+        Assert.That(await _mainPage!.IsPageDisplayed(), Is.True);
+    }
+
+
+    [TestCase("", "Please enter an integer")]
+    [TestCase("@@", "Please enter an integer")]
+    public async Task InvalidSymbolsCalculate(string number, string message)
+    {
+        await _mainPage!.Calculate(number);
+        Assert.That(await _mainPage.GetResult(), Is.EqualTo(_errorMessage));
+    }
+
+    [TestCase("0", "1")]
+    [TestCase("50", "3.0414093201713376e+64")]
+    public async Task CalculateValidNumber(string number, string result)
+    {
+        await _mainPage!.Calculate(number);
+        Assert.That(await _mainPage.GetResult(), Is.EqualTo(Result(number, result )));
+    }
+}
