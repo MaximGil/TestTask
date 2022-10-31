fbabb8f615c34fe542bced22e15fa870f572a07f
Maksim Gil <gil00maksim@gmail.com>
2022-10-31 10:14:57 +0300

    Add pages.

---
+++ Pages/BasePage.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Playwright;
+
+namespace Pages;
+
+public abstract class BasePage
+{
+    protected readonly IPage Page;
+
+    protected BasePage(IPage page)
+    {
+        Page = page;
+    }
+
+    public virtual async Task Goto(string url = "https://qainterview.pythonanywhere.com/" ) => await Page.GotoAsync(url);
+}
---
+++ Pages/MainPage.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Playwright;
+
+namespace Pages;
+
+public class MainPage : BasePage
+{
+    public MainPage(IPage page) : base(page)
+    {
+    }
+
+    private ILocator InputField => Page.Locator("[id='number']");
+    private ILocator CalculateButton => Page.Locator("//button[@id='getFactorial']");
+    private ILocator ResultLocator => Page.Locator("[id='resultDiv']");
+    private ILocator TitleLocator => Page.Locator("xpath=//h1[contains(@class, 'text-center')]");
+
+
+    public async Task<bool> IsPageDisplayed()
+    {
+        return await InputField.IsEnabledAsync() && await CalculateButton.IsEnabledAsync() &&
+               await ResultLocator.IsEnabledAsync() && await TitleLocator.IsEnabledAsync();
+    }
+
+
+}
